8d89dce47b031ec2deaf7207c5abfc9840cb3656
Seongseri <tjdtpfl@gmail.com>
2023-01-10 17:53:59 +0900

    메인 탭 완료

---
+++ screens/FeedsScreen.js
@@ -0,0 +1,26 @@
+import React, { useContext } from "react";
+import { StyleSheet, View, TextInput } from "react-native";
+import LogContext from "../contexts/LogContext";
+
+function FeedsScreen() {
+    const {text, setText} = useContext(LogContext);
+    return (
+        <View style={styles.block}>
+            <TextInput 
+                value={text}
+                onChangeText={setText}
+                placeholder="텍스트를 입력하세요."
+                style={styles.input}
+            />
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    input: {
+        padding: 16,
+        backgroundColor: 'white',
+    },
+});
+
+export default FeedsScreen;
